810bf36359f6418663a8dcb69adcf1b67e02be50
Saumya Ranjan <58022610+technicals-123@users.noreply.github.com>
2024-02-11 00:25:17 +0530

    Update convert_to_excel.sh

--- convert_to_excel.sh
+++ convert_to_excel.sh
@@ -1,22 +1,17 @@
+#!/bin/bash
+ 
+# Read CSV file and convert to Excel with table formatting
+python3 <<EOF
 import pandas as pd
  
 # Read CSV file
 df = pd.read_csv('system_info.csv')
  
-# Create Excel writer
-writer = pd.ExcelWriter('system_info.xlsx', engine='xlsxwriter')
- 
-# Write data to Excel with formatting
-df.to_excel(writer, index=False, sheet_name='Sheet1')
- 
-# Get the xlsxwriter workbook and worksheet objects
-workbook = writer.book
-worksheet = writer.sheets['Sheet1']
- 
-# Add border formatting to the whole table
-border_format = workbook.add_format({'border': 1})
-worksheet.conditional_format(0, 0, df.shape[0], df.shape[1] - 1,
-                             {'type': 'no_blanks', 'format': border_format})
- 
-# Close the Pandas Excel writer and output the Excel file
-writer.save()
+# Convert to Excel and save with table formatting
+with pd.ExcelWriter('system_info.xlsx', engine='xlsxwriter') as writer:
+    df.to_excel(writer, index=False, sheet_name='Sheet1')
+    workbook = writer.book
+    worksheet = writer.sheets['Sheet1']
+    table_format = workbook.add_format({'border': 1})
+    worksheet.add_table(0, 0, df.shape[0], df.shape[1] - 1, {'style': 'Table Style Medium 10', 'format': table_format})
+EOF
