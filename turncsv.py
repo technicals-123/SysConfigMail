1d6503975a9655fd302d1eba5c4c66cafae74bb1
Saumya Ranjan <58022610+technicals-123@users.noreply.github.com>
2024-02-12 15:48:17 +0530

    Create turncsv.py

---
+++ turncsv.py
@@ -0,0 +1,12 @@
+user_id=sys.argv[1]
+group_id=sys.argv[2]
+home_directory=sys.argv[3]
+shell=sys.argv[4]
+disk=sys.argv[5]
+size=sys.argv[6]
+free=sys.argv[7]
+interface=sys.argv[8]
+domain_name=sys.argv[9]
+ip_address=sys.argv[10]
+print(user_id)
+print(ip_address)
