60f91b6ed534c528025ca5f55aeb37d090d4c858
Saumya Ranjan <58022610+technicals-123@users.noreply.github.com>
2024-02-12 18:10:23 +0530

    Update turncsv.py

--- turncsv.py
+++ turncsv.py
@@ -23,8 +23,8 @@ user_info={
 #converting to dataframe
 user_info_df=pd.DataFrame([user_info])
 #disk_info_df=pd.DataFrame([disk_usuage_dict])
-print(user_info_dict)
-print(disk_usuage_dict)
+#print(user_info_dict)
+print(disk_info)
 print(user_info_df)
 #print(disk_info_df)
 
