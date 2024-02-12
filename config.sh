c380122fe121d0bd1c16d21855f6ee6953277522
Saumya Ranjan <58022610+technicals-123@users.noreply.github.com>
2024-02-12 16:58:43 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -64,4 +64,4 @@ echo "$network_info" | while read -r line; do
     fi
     echo "$user_id,$group_id,$home_directory,$shell,N/A,N/A,N/A,$interface,$domain_name,$ip_address" >> "$csv_file"
 done
-python turncsv.py "$user_id" "$group_id" "$home_directory" "$shell" "$disk_usuage" "$network_info"
+python turncsv.py "$user_id" "$group_id" "$home_directory" "$shell" "$disk_usage" "$network_info"
