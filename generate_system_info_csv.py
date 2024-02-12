8696569c738be54d0cbd28701725666e2676a2bd
Saumya Ranjan <58022610+technicals-123@users.noreply.github.com>
2024-02-12 14:50:38 +0530

    Update generate_system_info_csv.py

--- generate_system_info_csv.py
+++ generate_system_info_csv.py
@@ -3,7 +3,7 @@ import csv
 import socket
  
 # Execute the Bash script and capture its output
-bash_script_output = subprocess.Popen(['bash', 'your_script.sh'], stdout=subprocess.PIPE).communicate()[0].decode()
+bash_script_output = subprocess.Popen(['bash', 'config.sh'], stdout=subprocess.PIPE).communicate()[0].decode()
  
 # Parse the output of the Bash script
 output_lines = bash_script_output.splitlines()
