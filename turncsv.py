f76f642e63d3641b1e0d205dcfd461a9541052bf
Saumya Ranjan <58022610+technicals-123@users.noreply.github.com>
2024-02-12 18:08:28 +0530

    Update turncsv.py

--- turncsv.py
+++ turncsv.py
@@ -1,26 +1,30 @@
 import sys
 import pandas as pd
 # Create dictionaries to store the command-line arguments
-user_info_dict = {
-    "user_id": sys.argv[1],
-    "group_id": sys.argv[2],
-    "home_directory": sys.argv[3],
-    "shell": sys.argv[4],
+#user_info_dict = {
+#    "user_id": sys.argv[1],
+  #  "group_id": sys.argv[2],
+  #  "home_directory": sys.argv[3],
+  #  "shell": sys.argv[4],
   
-}
+#}
 disk_usage_str=sys.argv[5]
 disk_usuage_list=disk_usage_str.split()
-disk_usuage_dict={
+user_info={
+"user_id": sys.argv[1],
+ "group_id": sys.argv[2],
+  "home_directory": sys.argv[3],
+ "shell": sys.argv[4],
  "disk":disk_usuage_list[0],
  "size":disk_usuage_list[1],
  "free_space":disk_usuage_list[2]
+
 }
 #converting to dataframe
-# user_i
-user_info_df=pd.DataFrame([user_info_dict])
-disk_info_df=pd.DataFrame([disk_usuage_dict])
+user_info_df=pd.DataFrame([user_info])
+#disk_info_df=pd.DataFrame([disk_usuage_dict])
 print(user_info_dict)
 print(disk_usuage_dict)
 print(user_info_df)
-print(disk_info_df)
+#print(disk_info_df)
 
